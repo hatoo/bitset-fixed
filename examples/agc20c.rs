a47dc50e578e9921efdde81515ac9d9087429c22
hatoo <hato2000@gmail.com>
2019-06-01 19:17:11 +0900

    Add an exmaple for AGC20C

---
+++ examples/agc20c.rs
@@ -0,0 +1,20 @@
+use bitset_fixed::BitSet;
+use rand::prelude::*;
+
+fn main() {
+    let mut rng = StdRng::seed_from_u64(114514);
+
+    let n: Vec<usize> = (0..25).map(|_| rng.next_u32() as usize % 2000).collect();
+    let sum = n.iter().sum::<usize>();
+
+    let mut bitset = BitSet::new(sum + 1);
+    bitset.set(0, true);
+
+    for &x in &n {
+        bitset |= &(bitset.clone() << x);
+    }
+
+    let ans = ((sum + 1) / 2..).find(|&i| bitset[i]).unwrap();
+
+    println!("N = {:?}\nAnswer = {}", n, ans);
+}
